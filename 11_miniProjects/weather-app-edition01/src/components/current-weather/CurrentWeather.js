f0f4c776804c5ea6ee8bcdde71d456318277c995
Adel Ansari <adelansari@github.com>
2022-09-13 15:49:48 +0300

    weather forecast Edition 01

---
+++ 11_miniProjects/weather-app-edition01/src/components/current-weather/CurrentWeather.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import "./CurrentWeather.css"
+
+const CurrentWeather = ({ data }) => {
+  const WEEK_DAYS = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+  let today = new Date().toLocaleDateString('en-GB')
+  const dayInAWeek = new Date().getDay();
+
+  return (
+    <div className="weather">
+      <div className="top">
+        <div>
+          <p className="city">{data.city}</p>
+          <p className="weather-description">{data.weather[0].description}</p>
+        </div>
+        <img
+          alt="weather"
+          className="weather-icon"
+          src={`icons/${data.weather[0].icon}.png`}
+        />
+      </div>
+      <div className="bottom">
+        <p className="temperature">{Math.round(data.main.temp)}°C</p>
+        <div className="details">
+          <div className="parameter-row">
+            <span className="parameter-label top">{WEEK_DAYS[dayInAWeek-1]} , {today}</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Feels like</span>
+            <span className="parameter-value">
+              {Math.round(data.main.feels_like)}°C
+            </span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Wind</span>
+            <span className="parameter-value">{data.wind.speed} m/s</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Humidity</span>
+            <span className="parameter-value">{data.main.humidity}%</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Pressure</span>
+            <span className="parameter-value">{data.main.pressure} hPa</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+
+export default CurrentWeather
